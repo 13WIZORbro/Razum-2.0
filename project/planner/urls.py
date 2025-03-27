e0db2f56ba8f5bad219bc928f7563432879849f5
mojori <vbberchenko@gmail.com>
2025-03-27 18:05:18 +0300

    added paths to htmls

--- project/planner/urls.py
+++ project/planner/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("project/", views.project, name="project"),
+    path("project/task", views.task, name="task"),
+    path("project/task/subtask", views.subtask, name="subtask"),
 ]
--- project/planner/views.py
+++ project/planner/views.py
@@ -6,6 +6,11 @@ from django.contrib.auth.decorators import login_required
 def home(request):
     return render(request, "planner/homepage.html")
 
-
 def project(request):
-    return render(request, "planner/project_homepage.html")
+    return render(request, "planner/task_list.html")
+
+def task(request):
+    return render(request, "planner/subtask_list.html")
+
+def subtask(request):
+    return render(request, "planner/subtask_detail.html")
